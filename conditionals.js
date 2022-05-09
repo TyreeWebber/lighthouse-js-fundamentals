ec02fb6c109b127b5e4717c710e2028fbaab3ebf
Tyree Webber <webber.tyree@gmail.com>
2022-05-08 21:46:42 -0600

    Created if/else statement.

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,7 @@
-const raining = true;
 const cold = false;
 
-if (raining) {
-  console.log("Don't forget an umbrella!");
-}
-
 if (cold) {
-  console.log("Make sure to bring a jacket!");
-
+  console.log("Make sure you wear a scarf");
+} else {
+  console.log("You\'re fine as is.");
 }
-
-console.log("Ok, now you\'re ready to go outside!");
