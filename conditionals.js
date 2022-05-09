11e6a0ae973033de0ba67e1bc911ca369a97c797
Tyree Webber <webber.tyree@gmail.com>
2022-05-08 21:49:28 -0600

    Updated if/else statements.

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,10 @@
-const cold = false;
+const temperature = 12;
 
-if (cold) {
-  console.log("Make sure you wear a scarf");
+if (temperature < 0) {
+  console.log("Make sure you bring a jacket!");
+} else if (temperature < 15) {
+  console.log("You need sleeves.");
 } else {
-  console.log("You\'re fine as is.");
+  console.log("Shorts are fine.");
 }
+console.log("Now you\'re ready, see you when you get home!");
