d5bf94653c34647d3dd55d6a5a073e08b3bf7e7a
Tyree Webber <webber.tyree@gmail.com>
2022-05-08 21:45:06 -0600

    Updated if statement.

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,13 @@
 const raining = true;
+const cold = false;
 
 if (raining) {
-  console.log("Don't forget an umbrella");
+  console.log("Don't forget an umbrella!");
 }
+
+if (cold) {
+  console.log("Make sure to bring a jacket!");
+
+}
+
+console.log("Ok, now you\'re ready to go outside!");
